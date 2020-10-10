45ed2b6660a39eb85efe1bfccb6282aee03791d8
Dmitry Babanin <babanin.dima@gmail.com>
2020-10-10 12:57:06 +0300

    Init commit

---
+++ src/main/java/by/babanin/scc/SystemControlCohabitationApplication.java
@@ -0,0 +1,13 @@
+package by.babanin.scc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemControlCohabitationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemControlCohabitationApplication.class, args);
+    }
+
+}
